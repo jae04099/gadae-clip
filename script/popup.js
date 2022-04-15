a008fe23c3d3143b18b4b0b20fd1bf2e93ddfe59
gyurilee <jae04099@gmail.com>
2022-04-16 01:29:23 +0900

    add schedule component

--- script/popup.js
+++ script/popup.js
@@ -1,9 +1,35 @@
-// Initialize button with user's preferred color
-import { webList } from '../data/webList.js'
+import { webList, schedule } from '../data/webList.js'
+
 let cardWrap = document.getElementById("cardWrap");
+let scheduleArticle = document.getElementById("scheduleArticle");
+
 const mappedCards = webList.map((info, index) => {
   return `<li key=${index}><a href=${info.url} target="_blank"><img src='../images/icon/${info.icon}.png' alt=${info.title}><span>${info.title}</span></a></li>`
 }).join('')
 
 cardWrap.innerHTML = mappedCards;
 
+function leftPad(value) {
+  if (value >= 10) {
+    return value;
+  }
+  return `${value}`;
+}
+
+function toStringByFormatting(source, delimiter = '.') {
+  const month = leftPad(source.getMonth() + 1);
+  const day = leftPad(source.getDate());
+  return [month, day].join(delimiter);
+}
+
+const today = toStringByFormatting(new Date());
+const todayNum = Number(today)
+const mappedSchedule = schedule.filter((e) => Number(e.start_date) <= todayNum && Number(e.end_date) >= todayNum).map((info, index) => {
+  return `<li>${info.title}</li>`
+}).join('')
+
+scheduleArticle.innerHTML = `<p>${today}</p><ul id="scheduleDetail" class="schedule-detail"></ul>`
+
+let scheduleDetail = document.getElementById("scheduleDetail");
+
+scheduleDetail.innerHTML = mappedSchedule;
