941c9188e8954d7b51e0d4f27106fd368a6d86c0
gyurilee <jae04099@gmail.com>
2022-04-15 22:31:23 +0900

    edit

--- script/popup.js
+++ script/popup.js
@@ -1,5 +1,5 @@
 // Initialize button with user's preferred color
-import webList from '../data/webList.js'
+import { webList } from '../data/webList.js'
 let cardWrap = document.getElementById("cardWrap");
 const mappedCards = webList.map((info, index) => {
   return `<li key=${index}><a href=${info.url} target="_blank"><img src='../images/icon/${info.icon}.png' alt=${info.title}><span>${info.title}</span></a></li>`
