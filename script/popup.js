341372cc54e5c58aa9d2c92b6e279ce2287a8fe4
gyurilee <jae04099@gmail.com>
2022-04-16 00:42:51 +0900

    add schedule data

--- script/popup.js
+++ script/popup.js
@@ -5,4 +5,5 @@ const mappedCards = webList.map((info, index) => {
   return `<li key=${index}><a href=${info.url} target="_blank"><img src='../images/icon/${info.icon}.png' alt=${info.title}><span>${info.title}</span></a></li>`
 }).join('')
 
-cardWrap.innerHTML = mappedCards
+cardWrap.innerHTML = mappedCards;
+
