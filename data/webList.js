d7950aceafd68d00332daff3866847f60ac349b2
gyurilee <jae04099@gmail.com>
2022-04-15 22:09:53 +0900

    add cybercampus

--- data/webList.js
+++ data/webList.js
@@ -1,10 +1,11 @@
-const webList = [
+export const webList = [
     { 'id': 1, 'title': '공지사항', 'url': 'https://www.catholic.ac.kr/front/boardlist.do?cmsDirPkid=2053&cmsLocalPkid=1', 'icon': 'board' },
     { 'id': 2, 'title': '웹메일', 'url': 'https://mail.catholic.ac.kr/', 'icon': 'mail' },
     { 'id': 3, 'title': '중앙도서관', 'url': 'https://library.catholic.ac.kr/', 'icon': 'book' },
     { 'id': 4, 'title': '도서관잔여석', 'url': 'http://203.229.203.240/8080/Domian5_jythh.asp', 'icon': 'chair' },
     { 'id': 5, 'title': '트리니티', 'url': 'https://uportal.catholic.ac.kr/sso/jsp/sso/ip/login_form.jsp', 'icon': 'trinity' },
-    { 'id': 6, 'title': '증명서발급', 'url': 'https://www.catholic.ac.kr/www/campuslife47_1.html', 'icon': 'documents' },
+    // { 'id': 6, 'title': '증명서발급', 'url': 'https://www.catholic.ac.kr/www/campuslife47_1.html', 'icon': 'documents' },
+    { 'id': 6, 'title': '사이버캠퍼스', 'url': 'https://e-cyber.catholic.ac.kr/ilos/main/main_form.acl', 'icon': 'university' },
     { 'id': 7, 'title': '현장실습', 'url': 'http://co-op.catholic.ac.kr/', 'icon': 'career' },
     // { 'id': 8, 'title': '국제교류처', 'url': 'https://www.catholic.ac.kr/front/boardlist.do?cmsDirPkid=2053&cmsLocalPkid=1', 'icon': 'worldwide' },
     { 'id': 9, 'title': '학과바로가기', 'url': 'https://www.catholic.ac.kr/www/academinc1.html', 'icon': 'arrows' },
@@ -16,4 +17,3 @@ const webList = [
     { 'id': 15, 'title': '개설과목조회', 'url': 'https://haksa.catholic.ac.kr/front/opensubjectlist.do?cmsDirPkid=1710&cmsLocalPkid=0&rSiteGubun=11', 'icon': 'loupe' }
 ]
 
-export default webList;
