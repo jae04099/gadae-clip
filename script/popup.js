7010e998c75e2b6d0b908c51e7336ab5cd258abc
gyuri <jae04099@catholic.ac.kr>
2023-01-24 14:58:53 +0900

    update 2023

--- script/popup.js
+++ script/popup.js
@@ -4,7 +4,7 @@ let cardWrap = document.getElementById("cardWrap");
 let scheduleArticle = document.getElementById("scheduleArticle");
 
 const mappedCards = webList.map((info, index) => {
-  return `<li key=${index}><a href=${info.url} target="_blank"><img src='../images/icon/${info.icon}.png' alt=${info.title}><span>${info.title}</span></a></li>`
+  return `<li key=${index}><a href=${info.url} target="_blank"><div class="icon_wrap"><img src='../images/icon/${info.icon}.png' alt=${info.title}></div><span>${info.title}</span></a></li>`
 }).join('')
 
 cardWrap.innerHTML = mappedCards;
@@ -16,20 +16,34 @@ function leftPad(value) {
   return `${value}`;
 }
 
+function replaceDate(props) {
+  const tmpVal = props.year + '-' + props.start_date.replace('.', '-');
+  const valToNum = Date.parse(tmpVal);
+  return valToNum;
+}
+// start date <= target <= end date / 
+//YYYY-MM-DD
 function toStringByFormatting(source, delimiter = '.') {
   const month = leftPad(source.getMonth() + 1);
   const day = leftPad(source.getDate());
   return [month, day].join(delimiter);
 }
-
 const today = toStringByFormatting(new Date());
 const todayNum = Number(today)
-const mappedSchedule = schedule.filter((e) => Number(e.start_date) <= todayNum && Number(e.end_date) >= todayNum).map((info, index) => {
-  return `<li>${info.title}</li>`
-}).join('')
+const mappedSchedule = () => {
+  if (schedule.filter((e) => Number(e.start_date) <= todayNum && Number(e.end_date) >= todayNum).length) {
+    schedule.filter((e) => Number(e.start_date) <= todayNum && Number(e.end_date) >= todayNum).map((info, index) => {
+      { info.start_date == info.end_date ? (`<li key='${index}'><span>${info.start_date}</span>${info.title}</li>`) : (`<li key='${index}'><span>${info.start_date} - ${info.end_date}</span>${info.title}</li>`) }
+    }).join('')
+  } else {
+    const tmpObj = schedule.filter((e) => Number(e.start_date) > todayNum)[0]
+    if (tmpObj.start_date == tmpObj.end_date) return `<li><span>${tmpObj.start_date}</span>${tmpObj.title}</li>`
+    else return `<li><span>${tmpObj.start_date} - ${tmpObj.end_date}</span>${tmpObj.title}</li>`
+  }
+}
 
-scheduleArticle.innerHTML = `<p>${today}</p><ul id="scheduleDetail" class="schedule-detail"></ul>`
+scheduleArticle.innerHTML = `<ul id="scheduleDetail" class="schedule-detail"></ul>`
 
 let scheduleDetail = document.getElementById("scheduleDetail");
 
-scheduleDetail.innerHTML = mappedSchedule;
+scheduleDetail.innerHTML = mappedSchedule();
